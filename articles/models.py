a0769caba38b6063b4761d3f8cdb593f7053f12e
TareqMonwer <tareqmonwer137@gmail.com>
2021-08-28 23:21:54 +0600

    models, instance method & classmethods explained.

--- articles/models.py
+++ articles/models.py
@@ -3,13 +3,23 @@ from django.db.models.functions import Lower
 
 
 class Article(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField()
+    title = models.CharField(max_length=255, unique=True)
+    content = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.title
     
+    def is_spam(self):
+        if not self.content:
+            return True
+        return False
+
+    @classmethod
+    def get_titles_that_contains_word(cls, word='tutorial'):
+        articles = Article.objects.filter(
+            title__icontains=word
+        )
+        return articles
+
     class Meta:
         ordering = [Lower('title')]
-        verbose_name = "Post"
-        verbose_name_plural = "Posts"
